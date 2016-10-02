7e5550ee9275d1fc3969a7c700b2317d8de625e3
kmsheng <kmsh3ng@gmail.com>
2016-10-02 23:15:36 +0800

    move greet controller to home folder

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import express from 'express';
+import * as home from './home';
 
 const port = process.env.PORT || 3000;
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('hello world');
-});
+app.get('/', home.greet);
 
 app.listen(port, () => {
   console.log('Example app listening on port %s', port);
